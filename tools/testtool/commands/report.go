157766fa90a3e2c04a0983a53282b4832940ef3c
zhmurov <zhmurov@gmail.com>
2026-02-12 22:52:45 +0300

    FIx report URL

--- tools/testtool/commands/report.go
+++ tools/testtool/commands/report.go
@@ -9,7 +9,7 @@ import (
 
 var testingToken = ""
 
-const reportEndpoint = "https://app.manytask.org/api/itmo-go-2025-fall/report"
+const reportEndpoint = "https://app.manytask.org/api/go-2026-spring/report"
 
 func reportTestResults(token string, task string, userID string, failed bool) error {
 	if failed {
