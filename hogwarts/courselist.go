48a7d163c8b9a185c211d379fee5d8069cdec3e1
krasov <me@frostfree.ru>
2026-02-27 15:59:18 +0300

    solve courselist

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -2,6 +2,48 @@
 
 package hogwarts
 
+import "slices"
+
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	set := make(map[string]struct{}, len(prereqs)*2)
+	for c, deps := range prereqs {
+		set[c] = struct{}{}
+		for _, d := range deps {
+			set[d] = struct{}{}
+		}
+	}
+
+	all := make([]string, 0, len(set))
+	for c := range set {
+		all = append(all, c)
+	}
+	slices.Sort(all)
+
+	visited := make(map[string]bool, len(all))
+	visiting := make(map[string]bool, len(all))
+	order := make([]string, 0, len(all))
+
+	var dfs func(string)
+	dfs = func(c string) {
+		if visited[c] {
+			return
+		}
+		if visiting[c] {
+			panic("cycle detected")
+		}
+		visiting[c] = true
+
+		for _, d := range prereqs[c] {
+			dfs(d)
+		}
+
+		visiting[c] = false
+		visited[c] = true
+		order = append(order, c)
+	}
+
+	for _, c := range all {
+		dfs(c)
+	}
+	return order
 }
